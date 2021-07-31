0bd06cbe8d5a0865a9e84393efcf438270d32ba1
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-07-30 23:09:04 -0400

    redirect index, too.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default class IndexRoute extends Route {}
+export default class IndexRoute extends Route {
+  redirect() {
+    window.location.replace(`https://10002.unmute.nyc/`);
+  }
+}
