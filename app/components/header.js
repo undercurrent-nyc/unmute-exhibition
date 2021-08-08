0dc6716e3a32edff4cc87eec6e5a06d3e06a83ae
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-08-08 18:29:05 -0400

    Finish meeting.

--- app/components/header.js
+++ app/components/header.js
@@ -6,10 +6,12 @@ import { transition } from "d3-transition";
 
 export default class HeaderComponent extends Component {
 
+  colorChanges = 4;
+
   seconds = {
-    beforeDrop: 2000,
-    remainOpen: 3000,
-    dropDuration: 1000 / 3,
+    beforeDrop: 500,
+    remainOpenUnit: 5000,
+    dropDuration: 750 / 3,
     colorAfterDrop: 500,
     scrollUp: 500,
   }
@@ -27,55 +29,63 @@ export default class HeaderComponent extends Component {
     this.animate();
     setInterval(() => {
       this.animate();
-    }, this.seconds.beforeDrop + this.seconds.dropDuration + this.seconds.remainOpen + this.seconds.scrollUp);
+    }, this.seconds.beforeDrop + 
+      this.seconds.dropDuration * 3 + 
+      (this.seconds.remainOpenUnit * (2 + this.colorChanges)) + 
+      this.seconds.scrollUp);
   }
 
   animate(){
+    for(const times of [1, 2, 3]) {
+      this.dropText(select(`.unmute-header-text.u${times}`), times);
+    }
 
-    select(".unmute-header-date")
-      .transition()
-      .selectAll("path")
-      .attr("fill", "black");
+    this.colorText(this.seconds.beforeDrop + this.seconds.dropDuration * 3 + this.seconds.colorAfterDrop);
+    setTimeout(() => {
+      this.colorText(0);
+      let change = 0;
+      let interval = setInterval(() => {
+        if(change === this.colorChanges) {
+          this.blackenText();
+          clearInterval(interval);
+        } else {
+          this.colorText(0);
+          change += 1;
+        }
+      }, this.seconds.remainOpenUnit);
+    }, this.seconds.beforeDrop + this.seconds.dropDuration * 3 + this.seconds.colorAfterDrop + this.seconds.remainOpenUnit);
+  }
 
+  colorText(delay){
     this.isBlue = !this.isBlue;
-
     if(this.isBlue){
       this.color = this.blue;
     } else {
       this.color = this.red;
     }
 
-    for(const times of [1, 2, 3]) {
-      this.dropText(select(`.unmute-header-text.u${times}`), times);
-    }
+    selectAll(".header-colorable path")
+      .transition()
+      .delay(delay)
+      .attr("fill", this.color);
   }
 
   dropText(selection, times){
     selection.transition()
       .duration(this.seconds.scrollUp)
-      .attr("transform", "translate(0, 0)")
-      .selectAll("path")
-      .attr("fill", "black");
+      .attr("transform", "translate(0, 0)");
+
+    this.blackenText();
 
     selection.transition()
       .delay(this.seconds.scrollUp + this.seconds.beforeDrop)
       .ease(easeQuadOut)
       .duration(times * this.seconds.dropDuration)
       .attr("transform", `translate(0, ${times * 262})`)
-      .on("end", this.colorText(selection, times));
   }
 
-  colorText(selection, times){
-    if(times === 3){
-      selection.selectAll("path")
-        .transition()
-        .delay(this.seconds.colorAfterDrop + this.seconds.scrollUp + this.seconds.beforeDrop + this.seconds.dropDuration)
-        .attr("fill", this.color);
-      select(".unmute-header-date")
-        .selectAll("path")
-        .transition()
-        .delay(this.seconds.colorAfterDrop + this.seconds.scrollUp + this.seconds.beforeDrop + this.seconds.dropDuration)
-        .attr("fill", this.color);
-    }
+  blackenText(){
+    selectAll(".header-colorable path")
+      .attr("fill", "black");
   }
 }
